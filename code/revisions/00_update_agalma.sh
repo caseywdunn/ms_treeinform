36fd46dd4ecec438002e7a318d436c6351655521
August Guang <august.guang@gmail.com>
2018-03-14 11:00:34 -0400

    needs python2.7

--- code/revisions/00_update_agalma.sh
+++ code/revisions/00_update_agalma.sh
@@ -4,17 +4,16 @@
 # on Brown University's CCV computing cluster Oscar. At this time (February 19, 2018) this is the
 # latest version of trinity.
 
-module load conda # on your own computer you will need to have conda already installed. you can comment 
+#module load conda # on your own computer you will need to have conda already installed. you can comment 
 # out this line if that is the case
 
-conda create -n agalma
+conda create -n agalma python=2.7
 
-conda install -c conda-forge -n agalma pycairo
-conda install -c conda-forge -n agalma libgcc-ng
-conda install -c conda-forge -n agalma libstdcxx-ng
+conda install -c conda-forge -n agalma pycairo libgcc-ng libstdcxx-ng
 conda install -c dunnlab -n agalma agalma=1.0.1
 conda remove -n agalma agalma trinity
-conda install -c dunnlab -n agalma trinity=2.6.5
+#conda install -c dunnlab -n agalma trinity=2.6.5
+conda install -c dunnlab -n agalma trinity=2.5.1
 conda install -f -c dunnlab -n agalma agalma=1.0.1
 
 source activate agalma
